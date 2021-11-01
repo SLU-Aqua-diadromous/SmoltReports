726826771eeef049e4037096d78a8ecaa3e29565
Anders Kagervall <anders.kagervall@gmail.com>
2021-11-01 10:08:08 +0100

    Fix read_rdata to not use global variable rdata.

--- R/dataloaders.R
+++ R/dataloaders.R
@@ -1,36 +1,29 @@
 #' Read data used for Smolt mark recaputure model
 #'
-#' @param river
-#' @param year
-#' @param species
+#' @param fname character Default "RData_dump.RData" in current directory.
 #'
 #' @return
 #' Returns a data structure with the data from a smolt trap in river in year for species.
 #' @export
 #'
-#' @examples
 read_rdata <- function(fname = "RData_dump.RData") {
-  # fname <- system.file(file.path("smoltdata", river, year, species), "RData_dump.RData",
-  #                      package = "SmoltReports", mustWork = TRUE)
-  load(fname)
-  return(rdata)
+  myenv <- environment()
+  load(fname, envir = myenv)
+  if (!exists("rdata", myenv)) stop(paste0(fname, ' must contain list "rdata".'))
+  res <- get("rdata", envir = myenv)
+  return(res)
 }
 
 
 #' Read results from smolt model
 #'
-#' @param river
-#' @param year
-#' @param species
+#' @param fname character Default "model_results.xlsx" in current directory.
 #'
 #' @return
 #' Returns a data frame with the results from a smolt trap in river in year for species.
 #' @export
 #'
-#' @examples
 read_model_result <- function(fname = "model_results.xlsx") {
-  # fname <- system.file(file.path("smoltdata", river, year, species), "model_results.xlsx",
-  #                      package = "SmoltReports", mustWork = TRUE)
   d <- readxl::read_excel(fname , sheet = "stats",
                           col_types = c("text", "numeric",  "numeric", "numeric", "numeric",
                                         "numeric", "numeric", "numeric", "numeric"))
@@ -42,15 +35,14 @@ read_model_result <- function(fname = "model_results.xlsx") {
 
 #' Read the CU (smolt estimate coda) from the smolt model results
 #'
-#' @param river
-#' @param year
-#' @param species
+#' @param fname character Default "model_results.xlsx" in current directory.
 #'
 #' @return
-#' Returns a data frame with the columns iteration, chain1 and chain2.
+#' Returns a data frame with the columns iteration, chain1 and chain2. Chain1 and chain2 are
+#' samples (posterior) of the estimated CU (total smolt run).
+#'
 #' @export
 #'
-#' @examples
 read_CU <- function(fname = "model_results.xlsx") {
   # fname <- system.file(file.path("smoltdata", river, year, species), "model_results.xlsx",
   #                      package = "SmoltReports", mustWork = TRUE)
