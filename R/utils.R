697ffdf4bc5e39f1211563975fed2c7e12ae61be
Anders Kagervall <anders.kagervall@gmail.com>
2023-04-26 16:48:12 +0200

    New variant calculated by MODE. Variant Mode2 is probably buggy and should perhaps be removed

--- R/utils.R
+++ R/utils.R
@@ -23,8 +23,10 @@ MODE <- function(x) {
   cv <- CV(x)
   m1 <- round(stats::median(x) / (cv * cv + 1), digits = 0)
   m2 <- as.numeric(names(which.max(table(x))))
-  res <- c(m1, m2)
-  names(res) <- c("Mode1", "Mode2")
+  uniqv <- unique(x)
+  m3 <- uniqv[which.max(tabulate(match(x, uniqv)))]
+  res <- c(m1, m2, m3)
+  names(res) <- c("Mode1", "Mode2", "Mode3")
   return(res)
 }
 
