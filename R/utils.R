c49db95e3c7edeeae1ff12bd35c04086146672be
Anders Kagervall <anders.kagervall@gmail.com>
2024-01-30 15:40:53 +0100

    Update MODE() function

--- R/utils.R
+++ R/utils.R
@@ -14,11 +14,13 @@ CV <- function(x){
 #' Calculate the mode
 #'
 #' Calculate the mode of a numerical vector. The function will return a named
-#' numerical vector with the mode of x calculated in four different ways.
+#' numerical vector with the mode of x calculated in three different ways.
 #' Mode1 is stats::median(x) / (cv * cv + 1) as recommended by Atso but this
-#' method seems to give to low values if the cv is high. Mode4 is calculated with
-#' the density() function and is probably the one we should use on the posterior
-#' smolt estimate.
+#' method seems to give to low values if the cv is high. Mode2 is found by
+#' tabulating the data and returning the one with highest number of occurrences.
+#' Mode3 is calculated with the density() function and is probably the one we
+#  should use on the posterior smolt estimate.
+#'
 #'
 #' @param x numerical vector
 #'
@@ -29,13 +31,12 @@ CV <- function(x){
 MODE <- function(x) {
   cv <- CV(x)
   m1 <- stats::median(x) / (cv * cv + 1)
-  m2 <- as.numeric(names(which.max(table(x))))
   uniqv <- unique(x)
-  m3 <- uniqv[which.max(tabulate(match(x, uniqv)))]
+  m2 <- uniqv[which.max(tabulate(match(x, uniqv)))]
   d <- density(x)
-  m4 <- d$x[which.max(d$y)]
-  res <- c(m1, m2, m3, m4)
-  names(res) <- c("Mode1", "Mode2", "Mode3", "Mode4")
+  m3 <- d$x[which.max(d$y)]
+  res <- c(m1, m2, m3)
+  names(res) <- c("Mode1", "Mode2", "Mode3")
   return(res)
 }
 
